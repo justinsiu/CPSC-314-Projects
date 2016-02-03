2f76a7ff7e4a4e3d1de3afb7ec67e98172bc627a
justinsiu <anduril10@hotmail.com>
2016-02-03 01:25:02 -0800

    Claws in
    Claws squares in, adding hierarchy

--- P1/P1.js
+++ P1/P1.js
@@ -101,9 +101,13 @@ var pawGeometry = makeCube();
 var non_uniform_scale = new THREE.Matrix4().set(2,0,0,0, 0,0.5,0,-.25, 0,0,2,0.5, 0,0,0,1);
 pawGeometry.applyMatrix(non_uniform_scale);
 
+var clawGeometry = makeCube();
+var non_uniform_scale = new THREE.Matrix4().set(0.25,0,0,0, 0,0.25,0,0, 0,0,1,0.5, 0,0,0,1);
+clawGeometry.applyMatrix(non_uniform_scale);
+
 // MATRICES
 var torsoMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,2.5, 0,0,1,0, 0,0,0,1);
-
+var torsoFinalMatrix = torsoMatrix;
 
 
 // TO-DO: INITIALIZE THE REST OF YOUR MATRICES 
@@ -122,25 +126,65 @@ var noseFinalMatrix = new THREE.Matrix4().multiplyMatrices(headFinalMatrix,noseM
 var tailMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-2, 0,0,1,-12, 0,0,0,1);
 var tailFinalMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,tailMatrix);
 
-var legFLMatrix = new THREE.Matrix4().set(1,0,0,2.5, 0,1,0,-3, 0,0,1,2, 0,0,0,1);
+var legFLMatrix = new THREE.Matrix4().set(1,0,0,2.5, 0,1,0,-3, 0,0,1,3, 0,0,0,1);
 var legFLFinalMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,legFLMatrix);
 var pawFLMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-1, 0,0,1,0, 0,0,0,1);
 var pawFLFinalMatrix = new THREE.Matrix4().multiplyMatrices(legFLFinalMatrix,pawFLMatrix);
+var clawFL1Matrix = new THREE.Matrix4().set(1,0,0,-1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawFL1FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFLFinalMatrix, clawFL1Matrix);
+var clawFL2Matrix = new THREE.Matrix4().set(1,0,0,-0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawFL2FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFLFinalMatrix, clawFL2Matrix);
+var clawFL3Matrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawFL3FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFLFinalMatrix, clawFL3Matrix);
+var clawFL4Matrix = new THREE.Matrix4().set(1,0,0,0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawFL4FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFLFinalMatrix, clawFL4Matrix);
+var clawFL5Matrix = new THREE.Matrix4().set(1,0,0,1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawFL5FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFLFinalMatrix, clawFL5Matrix);
 
-var legFRMatrix = new THREE.Matrix4().set(1,0,0,-2.5, 0,1,0,-3, 0,0,1,2, 0,0,0,1);
+var legFRMatrix = new THREE.Matrix4().set(1,0,0,-2.5, 0,1,0,-3, 0,0,1,3, 0,0,0,1);
 var legFRFinalMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,legFRMatrix);
 var pawFRMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-1, 0,0,1,0, 0,0,0,1);
 var pawFRFinalMatrix = new THREE.Matrix4().multiplyMatrices(legFRFinalMatrix,pawFRMatrix);
+var clawFR1Matrix = new THREE.Matrix4().set(1,0,0,-1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawFR1FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFRFinalMatrix, clawFR1Matrix);
+var clawFR2Matrix = new THREE.Matrix4().set(1,0,0,-0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawFR2FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFRFinalMatrix, clawFR2Matrix);
+var clawFR3Matrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawFR3FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFRFinalMatrix, clawFR3Matrix);
+var clawFR4Matrix = new THREE.Matrix4().set(1,0,0,0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawFR4FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFRFinalMatrix, clawFR4Matrix);
+var clawFR5Matrix = new THREE.Matrix4().set(1,0,0,1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawFR5FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawFRFinalMatrix, clawFR5Matrix);
 
-var legRLMatrix = new THREE.Matrix4().set(1,0,0,2.5, 0,1,0,-3, 0,0,1,-2, 0,0,0,1);
+var legRLMatrix = new THREE.Matrix4().set(1,0,0,2.5, 0,1,0,-3, 0,0,1,-3, 0,0,0,1);
 var legRLFinalMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,legRLMatrix);
 var pawRLMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-1, 0,0,1,0, 0,0,0,1);
 var pawRLFinalMatrix = new THREE.Matrix4().multiplyMatrices(legRLFinalMatrix,pawRLMatrix);
+var clawRL1Matrix = new THREE.Matrix4().set(1,0,0,-1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawRL1FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRLFinalMatrix, clawRL1Matrix);
+var clawRL2Matrix = new THREE.Matrix4().set(1,0,0,-0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawRL2FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRLFinalMatrix, clawRL2Matrix);
+var clawRL3Matrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawRL3FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRLFinalMatrix, clawRL3Matrix);
+var clawRL4Matrix = new THREE.Matrix4().set(1,0,0,0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawRL4FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRLFinalMatrix, clawRL4Matrix);
+var clawRL5Matrix = new THREE.Matrix4().set(1,0,0,1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawRL5FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRLFinalMatrix, clawRL5Matrix);
 
-var legRRMatrix = new THREE.Matrix4().set(1,0,0,-2.5, 0,1,0,-3, 0,0,1,-2, 0,0,0,1);
+var legRRMatrix = new THREE.Matrix4().set(1,0,0,-2.5, 0,1,0,-3, 0,0,1,-3, 0,0,0,1);
 var legRRFinalMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,legRRMatrix);
 var pawRRMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-1, 0,0,1,0, 0,0,0,1);
 var pawRRFinalMatrix = new THREE.Matrix4().multiplyMatrices(legRRFinalMatrix,pawRRMatrix);
+var clawRR1Matrix = new THREE.Matrix4().set(1,0,0,-1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawRR1FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRRFinalMatrix, clawRR1Matrix);
+var clawRR2Matrix = new THREE.Matrix4().set(1,0,0,-0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawRR2FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRRFinalMatrix, clawRR2Matrix);
+var clawRR3Matrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawRR3FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRRFinalMatrix, clawRR3Matrix);
+var clawRR4Matrix = new THREE.Matrix4().set(1,0,0,0.5, 0,1,0,-.25, 0,0,1,1.5, 0,0,0,1);
+var clawRR4FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRRFinalMatrix, clawRR4Matrix);
+var clawRR5Matrix = new THREE.Matrix4().set(1,0,0,1.125, 0,1,0,-.25, 0,0,1,1, 0,0,0,1);
+var clawRR5FinalMatrix = new THREE.Matrix4().multiplyMatrices(pawRRFinalMatrix, clawRR5Matrix);
 
 
 // CREATE BODY
@@ -170,6 +214,21 @@ scene.add(legFL);
 var pawFL = new THREE.Mesh(pawGeometry,normalMaterial);
 pawFL.setMatrix(pawFLFinalMatrix);
 scene.add(pawFL);
+var clawFL1 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFL1.setMatrix(clawFL1FinalMatrix);
+scene.add(clawFL1);
+var clawFL2 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFL2.setMatrix(clawFL2FinalMatrix);
+scene.add(clawFL2);
+var clawFL3 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFL3.setMatrix(clawFL3FinalMatrix);
+scene.add(clawFL3);
+var clawFL4 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFL4.setMatrix(clawFL4FinalMatrix);
+scene.add(clawFL4);
+var clawFL5 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFL5.setMatrix(clawFL5FinalMatrix);
+scene.add(clawFL5);
 
 var legFR = new THREE.Mesh(legGeometry,normalMaterial);
 legFR.setMatrix(legFRFinalMatrix);
@@ -177,6 +236,21 @@ scene.add(legFR);
 var pawFR = new THREE.Mesh(pawGeometry,normalMaterial);
 pawFR.setMatrix(pawFRFinalMatrix);
 scene.add(pawFR);
+var clawFR1 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFR1.setMatrix(clawFR1FinalMatrix);
+scene.add(clawFR1);
+var clawFR2 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFR2.setMatrix(clawFR2FinalMatrix);
+scene.add(clawFR2);
+var clawFR3 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFR3.setMatrix(clawFR3FinalMatrix);
+scene.add(clawFR3);
+var clawFR4 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFR4.setMatrix(clawFR4FinalMatrix);
+scene.add(clawFR4);
+var clawFR5 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawFR5.setMatrix(clawFR5FinalMatrix);
+scene.add(clawFR5);
 
 var legRL = new THREE.Mesh(legGeometry,normalMaterial);
 legRL.setMatrix(legRLFinalMatrix);
@@ -184,6 +258,21 @@ scene.add(legRL);
 var pawRL = new THREE.Mesh(pawGeometry,normalMaterial);
 pawRL.setMatrix(pawRLFinalMatrix);
 scene.add(pawRL);
+var clawRL1 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRL1.setMatrix(clawRL1FinalMatrix);
+scene.add(clawRL1);
+var clawRL2 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRL2.setMatrix(clawRL2FinalMatrix);
+scene.add(clawRL2);
+var clawRL3 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRL3.setMatrix(clawRL3FinalMatrix);
+scene.add(clawRL3);
+var clawRL4 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRL4.setMatrix(clawRL4FinalMatrix);
+scene.add(clawRL4);
+var clawRL5 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRL5.setMatrix(clawRL5FinalMatrix);
+scene.add(clawRL5);
 
 var legRR = new THREE.Mesh(legGeometry,normalMaterial);
 legRR.setMatrix(legRRFinalMatrix);
@@ -191,6 +280,21 @@ scene.add(legRR);
 var pawRR = new THREE.Mesh(pawGeometry,normalMaterial);
 pawRR.setMatrix(pawRRFinalMatrix);
 scene.add(pawRR);
+var clawRR1 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRR1.setMatrix(clawRR1FinalMatrix);
+scene.add(clawRR1);
+var clawRR2 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRR2.setMatrix(clawRR2FinalMatrix);
+scene.add(clawRR2);
+var clawRR3 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRR3.setMatrix(clawRR3FinalMatrix);
+scene.add(clawRR3);
+var clawRR4 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRR4.setMatrix(clawRR4FinalMatrix);
+scene.add(clawRR4);
+var clawRR5 = new THREE.Mesh(clawGeometry,normalMaterial);
+clawRR5.setMatrix(clawRR5FinalMatrix);
+scene.add(clawRR5);
 
 // APPLY DIFFERENT JUMP CUTS/ANIMATIONS TO DIFFERNET KEYS
 // Note: The start of "U" animation has been done for you, you must implement the hiearchy and jumpcut.
@@ -406,14 +510,14 @@ function updateBody() {
                                             Math.sin(-p), 0,  Math.cos(-p), 0,
                                                 0,        0,      0,        1);
 
-      var setTailMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,tailMatrix);
-
-      var tailRotMatrix = new THREE.Matrix4().multiplyMatrices(rotateY, setTailMatrix);
+      var setTailMatrix = new THREE.Matrix4().multiplyMatrices(originMatrix,tailMatrix);
+      var setTailMatrix = new THREE.Matrix4().multiplyMatrices(rotateY,setTailMatrix);
+      var tailRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix, setTailMatrix);
       tail.setMatrix(tailRotMatrix);
 
     break
 
-    case(key == "V" && animate): // Tail turn right
+    case(key == "V" && animate): // Tail turn left
       var time = clock.getElapsedTime(); // t seconds passed since the clock started.
 
       if (time > time_end){
@@ -464,9 +568,17 @@ keyboard.domElement.addEventListener('keydown',function(event){
   else if(keyboard.eventMatches(event,"G")){
     (key == "G")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "G")} //head left
   else if(keyboard.eventMatches(event,"T")){
-    (key == "T")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "T")}
+    (key == "T")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "T")} //tail right
   else if(keyboard.eventMatches(event,"V")){
-    (key == "V")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "V")}
+    (key == "V")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "V")} //tail left
+  else if(keyboard.eventMatches(event,"N")){
+    (key == "N")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "N")} //nose fan
+  else if(keyboard.eventMatches(event,"S")){
+    (key == "S")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "S")} //swim
+  else if(keyboard.eventMatches(event,"E")){
+    (key == "E")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/8,1), key = "E")} //dig
+
+
 
 
   // TO-DO: BIND KEYS TO YOUR JUMP CUTS AND ANIMATIONS
